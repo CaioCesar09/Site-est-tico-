4e7644abc70b09a8eaede95640bc7fe50dd190ff
Caio Cesar <caiocesar318@GMAIL.COM>
2024-09-17 15:30:53 -0300

    Arquivos

---
+++ enviar.php
@@ -0,0 +1,13 @@
+﻿<?php
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$mensagem = $_POST['mensagem'];
+$corpo  = "Nome: ".$nome."\n";
+$corpo .= "Email: ".$email."\n";
+$corpo .= "Mensagem: ".$mensagem."\n";
+if(mail("MEU E-MAIL@GMAIL.com.br","Contato via SITE",$corpo,"From: E-MAIL QUE RECEBE@SITE.com.br")){
+  echo "<meta http-equiv='refresh' content='0;URL=contatos.html'>";
+} else {
+  echo("Erro ao enviar e-mail");
+}
+?>
